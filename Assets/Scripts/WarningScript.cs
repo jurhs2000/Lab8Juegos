d4c1055232b2b14b050006abc2db036040d42ff3
Julio Herrera <jurhs2000@gmail.com>
2020-05-23 22:21:21 -0600

    lab version 1.0

---
+++ Assets/Scripts/WarningScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarningScript : MonoBehaviour
+{
+    public Animator warningAnimator;
+    public Animator warningAnimator1;
+    public Animator warningAnimator2;
+    private bool isInRoom = false;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("WarningTrigger"))
+        {
+            if (isInRoom)
+            {
+                warningAnimator.Play("WarningOff");
+                warningAnimator1.Play("WarningOff");
+                warningAnimator2.Play("WarningOff");
+                isInRoom = false;
+            }
+            else
+            {
+                warningAnimator.Play("WarningAnimator");
+                warningAnimator1.Play("WarningAnimator");
+                warningAnimator2.Play("WarningAnimator");
+                isInRoom = true;
+            }
+        }
+    }
+}
