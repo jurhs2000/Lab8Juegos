1e67b5ebf49331a19806da6000c8f108cede88a9
Julio Herrera <jurhs2000@gmail.com>
2020-05-23 20:27:07 -0600

    tags added

---
+++ Assets/Scripts/ObjectsNameRay.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsNameRay : MonoBehaviour
+{
+    public GameObject canvas;
+    public UnityEngine.UI.Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hitInfo;
+        Vector3 rayOrigin = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+        if (Physics.Raycast(rayOrigin, transform.forward, out hitInfo, 6f))
+        {
+            if (hitInfo.collider.CompareTag("Target"))
+            {
+                canvas.SetActive(true);
+                text.text = hitInfo.collider.gameObject.name;
+            } else
+            {
+                canvas.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TurnFlashlight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnFlashlight : MonoBehaviour
+{
+    public Light light;
+    public AudioSource audio;
+    private float intensity = 0;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            audio.Play();
+            if (intensity == 0)
+                intensity = 5.05f;
+            else
+                intensity = 0;
+            light.intensity = intensity;
+        }
+    }
+}
