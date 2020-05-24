604df18af3fb880a69196d2d7c48bed3c74d97bc
Julio Herrera <jurhs2000@gmail.com>
2020-05-23 23:30:02 -0600

    version final 1.2

---
+++ Assets/Scripts/EnemyAgent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class EnemyAgent : MonoBehaviour
+{
+    private Vector3 startPos;
+    public GameObject player;
+    private bool isHunting = false;
+    public NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isHunting)
+        {
+            agent.SetDestination(player.transform.position);
+        }
+    }
+
+    public void hunt()
+    {
+        isHunting = true;
+    }
+
+    public void setOrigin()
+    {
+        transform.position = startPos;
+        isHunting = false;
+    }
+}
--- Assets/Scripts/WarningScript.cs
+++ Assets/Scripts/WarningScript.cs
@@ -7,7 +7,9 @@ public class WarningScript : MonoBehaviour
     public Animator warningAnimator;
     public Animator warningAnimator1;
     public Animator warningAnimator2;
+    public GameObject slender;
     private bool isInRoom = false;
+    public EnemyAgent enemy;
 
     private void OnTriggerExit(Collider other)
     {
@@ -19,6 +21,7 @@ public class WarningScript : MonoBehaviour
                 warningAnimator1.Play("WarningOff");
                 warningAnimator2.Play("WarningOff");
                 isInRoom = false;
+                enemy.setOrigin();
             }
             else
             {
@@ -26,6 +29,8 @@ public class WarningScript : MonoBehaviour
                 warningAnimator1.Play("WarningAnimator");
                 warningAnimator2.Play("WarningAnimator");
                 isInRoom = true;
+                slender.SetActive(true);
+                enemy.hunt();
             }
         }
     }
